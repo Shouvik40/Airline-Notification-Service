e3c4e0159fd9dc09f21b49da75d58f12615b975d
Shouvik40 <105535909+Shouvik40@users.noreply.github.com>
2023-09-23 14:08:23 +0530

    Added create ticket api

--- src/index.js
+++ src/index.js
@@ -19,14 +19,4 @@ app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-  // try {
-  //   const response = await mailsender.sendMail({
-  //     from: ServerConfig.GMAIL_EMAIL,
-  //     to: "cetel40762@armablog.com",
-  //     text: "Yes,it is working",
-  //   });
-  //   console.log(response);
-  // } catch (error) {
-  //   console.log(error);
-  // }
 });
